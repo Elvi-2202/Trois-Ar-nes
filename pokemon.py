a3d0ce3967860c6ec470e9f89f4caae9aa32a5c5
Elvi-2202 <156439254+Elvi-2202@users.noreply.github.com>
2025-10-29 21:05:09 +0100

    modification du code de pokemon

--- pokemon.py
+++ pokemon.py
@@ -2,7 +2,7 @@ import random
 from abc import ABC, abstractmethod
 
 class Pokemon(ABC):
-    def __init__(self, nom: str, type_pokemon: str, niveau: int, pv_max: int, attaque_base: int):
+    def __init__(self, nom, type_pokemon, niveau, pv_max, attaque_base):
         self.nom = nom
         self.type = type_pokemon
         self.niveau = niveau
@@ -11,19 +11,17 @@ class Pokemon(ABC):
         self.attaque_base = attaque_base
         self.experience = 0
         self.xp_pour_prochain_niveau = self.calculer_xp_necessaire()
-        self.evolution_niveau = 0  
+        self.evolution_niveau = 0 
         self.prochain_nom = ""
         self.prochaine_classe = None
 
-    def calculer_xp_necessaire(self) -> int:
-        return self.niveau * 10 + 50
+    def calculer_xp_necessaire(self) -> int: return self.niveau * 10 + 50
 
     def monter_niveau(self) -> 'Pokemon':
         self.niveau += 1
         print(f"\n✨ {self.nom} atteint le Niveau {self.niveau} !")
         
-        pv_gain = random.randint(4, 6)
-        atk_gain = random.randint(1, 3)
+        pv_gain, atk_gain = random.randint(4, 6), random.randint(1, 3)
         self.pv_max += pv_gain
         self.attaque_base += atk_gain
         self.points_de_vie = self.pv_max
@@ -34,29 +32,23 @@ class Pokemon(ABC):
         
         return self.verifier_evolution()
 
-    def gagner_xp(self, xp_gagne: int) -> 'Pokemon':
-        if self.niveau >= 100:
-             return self
-             
+    def gagner_xp(self, xp_gagne) -> 'Pokemon':
+        if self.niveau >= 100: return self
         self.experience += xp_gagne
         
         nouvelle_evolution = self
         while self.experience >= self.xp_pour_prochain_niveau:
             nouvelle_evolution = self.monter_niveau() 
-            if nouvelle_evolution != self:
-                return nouvelle_evolution 
+            if nouvelle_evolution != self: return nouvelle_evolution 
                 
         return nouvelle_evolution
 
-    def evoluer(self, nouvelle_nom: str, nouveau_type_classe) -> 'Pokemon':
+    def evoluer(self, nouvelle_nom, nouveau_type_classe) -> 'Pokemon':
         """Effectue l'évolution du Pokémon."""
         print(f"\n🎉 Félicitations ! Votre {self.nom} est en train d'évoluer...")
         
         nouveau_pokemon = nouveau_type_classe(
-            nouvelle_nom,
-            self.niveau,
-            self.pv_max + 10, 
-            self.attaque_base + 5 
+            nouvelle_nom, self.niveau, self.pv_max + 10, self.attaque_base + 5 
         )
         nouveau_pokemon.experience = self.experience
         nouveau_pokemon.points_de_vie = self.points_de_vie
@@ -69,26 +61,21 @@ class Pokemon(ABC):
             return self.evoluer(self.prochain_nom, self.prochaine_classe)
         return self
         
-    def est_ko(self) -> bool:
-        return self.points_de_vie <= 0
+    def est_ko(self) -> bool: return self.points_de_vie <= 0
         
     @abstractmethod
-    def calculer_degats_speciaux(self, autre_pokemon: 'Pokemon') -> float:
-        pass
+    def calculer_degats_speciaux(self, autre_pokemon: 'Pokemon') -> float: pass
     
     def attaquer(self, autre_pokemon: 'Pokemon'):
-        if self.est_ko():
-            return
+        if self.est_ko(): return
 
         degats_bruts = self.attaque_base + random.randint(1, 5)
         multiplicateur = self.calculer_degats_speciaux(autre_pokemon)
         degats_finaux = int(degats_bruts * multiplicateur)
         
         message_type = ""
-        if multiplicateur > 1.0:
-            message_type = " (Super efficace ! )"
-        elif multiplicateur < 1.0:
-            message_type = " (Pas très efficace... )"
+        if multiplicateur > 1.0: message_type = " (Super efficace ! )"
+        elif multiplicateur < 1.0: message_type = " (Pas très efficace... )"
             
         print(f"   {self.nom} ({self.type}) attaque {autre_pokemon.nom} et inflige {degats_finaux} dégâts{message_type}!")
         
@@ -100,73 +87,55 @@ class Pokemon(ABC):
 
 
 class PokemonEvoFeu(Pokemon):
-     def __init__(self, nom: str, niveau: int, pv_max: int, attaque_base: int):
+    def __init__(self, nom, niveau, pv_max, attaque_base):
         super().__init__(nom, "Feu", niveau, pv_max, attaque_base)
-        self.evolution_niveau = 0 
-        
-     def verifier_evolution(self): return self 
-     
-     def calculer_degats_speciaux(self, autre_pokemon: Pokemon) -> float:
+    def verifier_evolution(self): return self 
+    def calculer_degats_speciaux(self, autre_pokemon: Pokemon) -> float:
         if autre_pokemon.type == "Plante": return 2.0
         elif autre_pokemon.type == "Eau": return 0.5
         return 1.0
 
-
 class PokemonEvoEau(Pokemon):
-     def __init__(self, nom: str, niveau: int, pv_max: int, attaque_base: int):
+    def __init__(self, nom, niveau, pv_max, attaque_base):
         super().__init__(nom, "Eau", niveau, pv_max, attaque_base)
-        self.evolution_niveau = 0 
-        
-     def verifier_evolution(self): return self 
-     
-     def calculer_degats_speciaux(self, autre_pokemon: Pokemon) -> float:
+    def verifier_evolution(self): return self 
+    def calculer_degats_speciaux(self, autre_pokemon: Pokemon) -> float:
         if autre_pokemon.type == "Feu": return 2.0
         elif autre_pokemon.type == "Plante": return 0.5
         return 1.0
 
 class PokemonEvoPlante(Pokemon):
-     def __init__(self, nom: str, niveau: int, pv_max: int, attaque_base: int):
+    def __init__(self, nom, niveau, pv_max, attaque_base):
         super().__init__(nom, "Plante", niveau, pv_max, attaque_base)
-        self.evolution_niveau = 0 
-        
-     def verifier_evolution(self): return self 
-     
-     def calculer_degats_speciaux(self, autre_pokemon: Pokemon) -> float:
+    def verifier_evolution(self): return self 
+    def calculer_degats_speciaux(self, autre_pokemon: Pokemon) -> float:
         if autre_pokemon.type == "Eau": return 2.0
         elif autre_pokemon.type == "Feu": return 0.5
         return 1.0
 
+# Classes de base (peuvent évoluer)
 class PokemonFeu(Pokemon):
-    def __init__(self, nom: str, niveau: int, pv_max: int, attaque_base: int):
+    def __init__(self, nom, niveau, pv_max, attaque_base):
         super().__init__(nom, "Feu", niveau, pv_max, attaque_base)
-        self.evolution_niveau = 15
-        self.prochain_nom = "Pyroli" 
-        self.prochaine_classe = PokemonEvoFeu
-
+        self.evolution_niveau = 15; self.prochain_nom = "Pyroli"; self.prochaine_classe = PokemonEvoFeu
     def calculer_degats_speciaux(self, autre_pokemon: Pokemon) -> float:
         if autre_pokemon.type == "Plante": return 2.0
         elif autre_pokemon.type == "Eau": return 0.5
         return 1.0
         
 class PokemonEau(Pokemon):
-    def __init__(self, nom: str, niveau: int, pv_max: int, attaque_base: int):
+    def __init__(self, nom, niveau, pv_max, attaque_base):
         super().__init__(nom, "Eau", niveau, pv_max, attaque_base)
-        self.evolution_niveau = 15
-        self.prochain_nom = "Aquali"
-        self.prochaine_classe = PokemonEvoEau 
-        
+        self.evolution_niveau = 15; self.prochain_nom = "Aquali"; self.prochaine_classe = PokemonEvoEau 
     def calculer_degats_speciaux(self, autre_pokemon: Pokemon) -> float:
         if autre_pokemon.type == "Feu": return 2.0
         elif autre_pokemon.type == "Plante": return 0.5
         return 1.0
 
 class PokemonPlante(Pokemon):
-    def __init__(self, nom: str, niveau: int, pv_max: int, attaque_base: int):
+    def __init__(self, nom, niveau, pv_max, attaque_base):
         super().__init__(nom, "Plante", niveau, pv_max, attaque_base)
-        self.evolution_niveau = 15
-        self.prochain_nom = "Phyllali" 
-        self.prochaine_classe = PokemonEvoPlante 
-
+        self.evolution_niveau = 15; self.prochain_nom = "Phyllali"; self.prochaine_classe = PokemonEvoPlante 
     def calculer_degats_speciaux(self, autre_pokemon: Pokemon) -> float:
         if autre_pokemon.type == "Eau": return 2.0
         elif autre_pokemon.type == "Feu": return 0.5
